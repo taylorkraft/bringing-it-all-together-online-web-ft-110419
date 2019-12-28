7548a4cbff0965f133cfc5fa276ba2472bac48df
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-28 05:17:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(id = nil, name:, breed:)
+  def initialize(arg)
+    arg = id = nil, name:, breed:
     @id = id
     @name = name
     @breed = breed
