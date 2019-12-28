16326190b680d452aab498c4fad168d63f41833b
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-28 15:34:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attr)
-    attributes.each { |key, value|
+    attr.each { |key, value|
        self.send(("#{key}="), value)}
     self.id ||= nil
   end
